5034caf7b81b22701513f0d481a934e389fedb39
gire-tejas-3 <giretejas@gmail.com>
2024-06-29 16:44:51 +0530

    Temp Commit

--- src/com/ecommerce/application/Test2.java
+++ src/com/ecommerce/application/Test2.java
@@ -5,6 +5,16 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
+		System.out.println("Temp");
 	}
 
 }

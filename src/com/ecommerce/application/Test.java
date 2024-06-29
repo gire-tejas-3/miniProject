972879920a39fabe173599a9942d207de28fdede
gire-tejas-3 <giretejas@gmail.com>
2024-06-29 16:29:51 +0530

    Initial Commit

---
+++ src/com/ecommerce/application/Test.java
@@ -0,0 +1,10 @@
+package com.ecommerce.application;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ecommerce ");
+	}
+
+}

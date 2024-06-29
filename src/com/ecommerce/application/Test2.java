a811ef45d456adcd7467462ffe045a35f767b34d
gire-tejas-3 <giretejas@gmail.com>
2024-06-29 16:38:19 +0530

    Second Commit

---
+++ src/com/ecommerce/application/Test2.java
@@ -0,0 +1,10 @@
+package com.ecommerce.application;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Temp");
+	}
+
+}

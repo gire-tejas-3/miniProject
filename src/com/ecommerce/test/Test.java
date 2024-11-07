460cac99fe89bf4e2ed10646ad540371c16c7207
gire-tejas-3 <giretejas@gmail.com>
2024-11-07 18:23:48 +0530

    Cherry picked

--- src/com/ecommerce/test/Test.java
+++ src/com/ecommerce/test/Test.java
@@ -5,8 +5,32 @@ public class Test {
 	void method1() {
 		System.out.println("Method No 1");
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> 627be93 (Method 1)
+	void method2() {
+		System.out.println("Method No 2");
+	}
+
+	void method3() {
+		System.out.println("Method No 3");
+	}
+
+	void method4() {
+		System.out.println("Method No 4");
+	}
+
+	void method5() {
+		System.out.println("Method No 5");
+	}
+
+	void method6() {
+		System.out.println("Method No 6");
+	}
+
+	void method7() {
+		System.out.println("Method No 7");
+	}
+
+	void method8() {
+		System.out.println("Method No 8");
+	}
 }

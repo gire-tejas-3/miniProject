6fd26770c39a0e5051ee11a76cfc962b1725602c
gire-tejas-3 <giretejas@gmail.com>
2024-11-07 17:08:02 +0530

    Resolved Conflict

--- src/com/ecommerce/test/Test.java
+++ src/com/ecommerce/test/Test.java
@@ -6,31 +6,4 @@ public class Test {
 		System.out.println("Method No 1");
 	}
 
-	void method2() {
-		System.out.println("Method No 2");
-	}
-
-	void method3() {
-		System.out.println("Method No 3");
-	}
-
-	void method4() {
-		System.out.println("Method No 4");
-	}
-
-	void method5() {
-		System.out.println("Method No 5");
-	}
-
-	void method6() {
-		System.out.println("Method No 6");
-	}
-
-	void method7() {
-		System.out.println("Method No 7");
-	}
-
-	void method8() {
-		System.out.println("Method No 8");
-	}
 }
